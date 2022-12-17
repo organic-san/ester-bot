4e8ebe77573223d4f8d3c43ff1e18bf1a9f41b0e
organic_san <yuki.0710.happy@gmail.com>
2022-12-18 00:04:42 +0800

    add: 將表情符號轉換功能移到/emoji-convert

--- commands/emoji-convert.js
+++ commands/emoji-convert.js
@@ -5,7 +5,12 @@ const guild = require('../class/guildInformation');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('emoji-convert')
-		.setDescription('設定表情符號轉換功能的開關'),
+		.setDescription('表情符號轉換功能，轉換後的訊息會用跟你的頭像一樣的機器人代為發送')
+        .addStringOption(opt => 
+            opt.setName("message")
+            .setDescription('要轉換的訊息，在這裡輸入的:emoji:都會轉換成真正的表情符號!')
+            .setRequired(true)
+        ),
 	tag: "guildInfo",
     /**
      * 
@@ -13,6 +18,60 @@ module.exports = {
      * @param {guild.GuildInformation} guildInformation 
      */
 	async execute(interaction, guildInformation) {
+        const message = interaction.options.getString('message');
+        if(interaction.channel.permissionsFor(interaction.client.user).has(Discord.Permissions.FLAGS.MANAGE_WEBHOOKS)) {
+            if(!interaction.channel.isThread() && guildInformation.emojiTrans){
+                const notEmoji = message.split(/:\w+:/g);
+                const isEmoji = [...message.matchAll(/:\w+:/g)];
+                isEmoji.forEach((v, i) => isEmoji[i] = v[0]);
+
+                if(isEmoji.length > 0) {
+                    isEmoji.forEach((emoji, index) => {
+                        if(!emoji) return;
+                        if(notEmoji[index].endsWith('<')) return;
+                        if(notEmoji[index].endsWith('<a')) return;
+                        let find = interaction.client.emojis.cache.find(e => e.name === emoji.slice(1, emoji.length - 1));
+                        if(!find) find = interaction.client.emojis.cache.find(e => e.name.includes(emoji.slice(1, emoji.length - 1)));
+                        if(!find) find = interaction.client.emojis.resolve(emoji.slice(1, emoji.length - 1));
+                        if(find) {
+                            isEmoji[index] = find.toString();
+                        } else {
+                            isEmoji[index] = emoji;
+                        }
+                    })
+    
+                    interaction.reply({content: "訊息已送出!", ephemeral: true}).catch((err) => console.log(err));
+                    //console.log("isCommand: true: isEmojiWebhook");
+
+                    let words = [];
+                    for(let i = 0; i < notEmoji.length * 2 - 1; i++)
+                        i % 2 ? words.push(isEmoji[(i-1)/2]) : words.push(notEmoji[i/2]);
+                    words = words.join("");
+
+                    const webhooks = await interaction.channel.fetchWebhooks();
+                    let webhook = webhooks.find(webhook => webhook.owner.id === interaction.client.user.id);
+                    if(!webhook) {
+                        msg.channel.createWebhook(interaction.member.displayName, {
+                            avatar: interaction.user.displayAvatarURL({dynamic: true, format: "png"})
+                        })
+                            .then(webhook => webhook.send({content: words, allowedMentions: {repliedUser: false}}))
+                            .catch(console.error);
+                    } else {
+                        await webhook.edit({
+                            name: interaction.member.displayName,
+                            avatar: interaction.user.displayAvatarURL({dynamic: true, format: "png"})
+                        })
+                            .then(webhook => webhook.send({content: words, allowedMentions: {repliedUser: false}}))
+                            .catch(console.error);
+                    }
+                    //if(msg.deletable) msg.delete().catch((err) => console.log(err));
+                    return;
+                    
+                }
+            }
+        }
+
+        /*
         if (!interaction.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_MESSAGES)) return;
         const cmd = await interaction.reply({
             content: `自動表情符號轉換功能 目前狀態: ${guildInformation.emojiTrans ? "開啟" : "停用"}`, 
